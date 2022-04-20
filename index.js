b3193aa4a16d1f6b84bd9d0ee0475d732b779a22
Taras <angrywolf099@gmail.com>
2022-04-20 18:57:39 +0300

    умова/цикл

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt("Скільки фільмів ви вже подивились?", "");
+const numberOfFilms = prompt("Скільки фільмів ви вже подивились?", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,14 +8,40 @@ const personalMovieDB = {
   privat: false,
 };
 
-const nameFilm1 = prompt("Один з останніх переглянутих фільмів", ""),
-  ratingFilm1 = prompt("Яку б ви оцінку йому поставили?", ""),
-  nameFilm2 = prompt("Один з останніх переглянутих фільмів", ""),
-  ratingFilm2 = prompt("Яку б ви оцінку йому поставили?", "");
+// const nameFilm1 = prompt("Один з останніх переглянутих фільмів", ""),
+//   ratingFilm1 = prompt("Яку б ви оцінку йому поставили?", ""),
+//   nameFilm2 = prompt("Один з останніх переглянутих фільмів", ""),
+//   ratingFilm2 = prompt("Яку б ви оцінку йому поставили?", "");
+
+// personalMovieDB.movies[nameFilm1] = ratingFilm1;
+// personalMovieDB.movies[nameFilm2] = ratingFilm2;
+
+//  заміняєм повторення питань на цикл (внизу)
+
+// for (let i = 0; i < 2; i++) {
+//   const nameFilm1 = prompt("Один з останніх переглянутих фільмів", ""),
+//     ratingFilm1 = prompt("Яку б ви оцінку йому поставили?", "");
+
+// // умови на пусту строку , відміну і к-ть символів більше ніж 50
+
+//   if (nameFilm1 != null && ratingFilm1 != null&& nameFilm1!='' && ratingFilm1!='' && nameFilm1.length<50){
+//     personalMovieDB.movies[nameFilm1] = ratingFilm1;
+// console.log('done');
+//   } else{
+//     console.log('error');
+//     // повертає один цикл назад, і воно запускає ше раз питання
+//     i--
+//   }
+// }
+// console.log(personalMovieDB);
 
 
-personalMovieDB.movies[nameFilm1]=ratingFilm1;
-personalMovieDB.movies[nameFilm2]=ratingFilm2
-
-
-  console.log(personalMovieDB);
+if (personalMovieDB.count <= 10) {
+  alert("Мало фільмів");
+} else if (personalMovieDB.count < 30) {
+  alert("Середна к-сть фільмів");
+} else if (personalMovieDB.count >=30) {
+  alert("кіноман");
+} else{
+  alert('error')
+}
